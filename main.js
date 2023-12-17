6caa4905dac0ef68d4a962f3809d30156c172b81
ryujinzz <kdo2240k@naver.com>
2023-12-17 21:00:20 +0900

    토글이 왜 안 되는지 모르겠다 어렵네

--- main.js
+++ main.js
@@ -3,3 +3,13 @@
  * 1. 선택된 element에 active 클래스 추가
  * 2. 선택되지 않은 element는 active클래스 제거
  */
+
+const container = document.querySelector(".container");
+const div = container.querySelectorAll("div");
+
+function handleClick(e) {
+  e.preventDefault();
+  div.classList.toggle("active");
+}
+
+container.addEventListener("click", handleClick);
