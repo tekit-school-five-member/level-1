44e1b6458b2306e548af17950e0e9002a0f0c5ab
clapsheep <sktndid1203@naver.com>
2023-12-23 22:09:14 +0900

    [level 1 완성]

--- main.js
+++ main.js
@@ -3,3 +3,26 @@
  * 1. 선택된 element에 active 클래스 추가
  * 2. 선택되지 않은 element는 active클래스 제거
  */
+
+const container = document.querySelector(".container");
+const panelList = Array.from(container.children);
+
+function handleClick(e) {
+  const target = e.target;
+  const panel = target.closest(".panel");
+
+  // forEach 사용하여 active 클래스 제거하기
+  /*  panelList.forEach((li) => {
+    li.classList.remove("active");
+  });*/
+
+  // for of 문 사용하여 active 클래스 제거하기
+  for (let li of panelList) {
+    li.classList.remove("active");
+  }
+
+  // 선택한 노드에 active 클래스 추가하기
+  if (!panel) return;
+  panel.classList.add("active");
+}
+container.addEventListener("click", handleClick);
